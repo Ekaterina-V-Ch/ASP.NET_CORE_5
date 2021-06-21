08a2546e049e0bf12f3098186c23768b0628a334
Ekaterina-V-Ch <katerinachernenko13@gmail.com>
2021-06-21 15:56:27 +0300

    main

---
+++ Newsfeed/Newsfeed/Models/SmartphonesNews.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Newsfeed.Models
+{
+    public class SmartphonesNews
+    {
+        [Key]
+        public int Id { get; set; }
+        public string News { get; set; }
+        public int Data { get; set; }
+    }
+}
